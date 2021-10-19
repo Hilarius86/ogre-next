23e874776817372dafad709cb42f3f68551a20fb
Felix Tschirschwitz <felix.tschirschwitz@card-1.com>
2021-10-19 10:42:30 +0200

    Changed Build Script for card_1

--- Scripts/BuildScripts/output/build_ogre_Visual_Studio_15_2017_x64.bat
+++ Scripts/BuildScripts/output/build_ogre_Visual_Studio_15_2017_x64.bat
@@ -6,6 +6,9 @@ set OGRE_BRANCH_NAME=v2-2
 set GENERATOR="Visual Studio 15 2017"
 set PLATFORM=x64
 
+set BUILD_FOLDER = c:\entwicklung\ogre-next_2_2\
+set OGRE_DIRECTORY_SRC = c:\entwicklung\card_1\ogre-next\
+
 set CMAKE_BIN_x86="C:\Program Files (x86)\CMake\bin\cmake.exe"
 set CMAKE_BIN_x64="C:\Program Files\CMake\bin\cmake.exe"
 IF EXIST %CMAKE_BIN_x64% (
@@ -23,8 +26,10 @@ IF EXIST %CMAKE_BIN_x64% (
 )
 echo Using CMake at %CMAKE_BIN%
 
-mkdir Ogre
-cd Ogre
+cd ../../../../..
+
+mkdir ogre-next_2_2
+cd ogre-next_2_2
 IF NOT EXIST ogre-next-deps (
 	mkdir ogre-next-deps
 	echo --- Cloning ogre-next-deps ---
@@ -35,30 +40,19 @@ IF NOT EXIST ogre-next-deps (
 cd ogre-next-deps
 mkdir build
 cd build
-echo --- Building ogre-next-deps ---
-%CMAKE_BIN% -G %GENERATOR% -A %PLATFORM% ..
-%CMAKE_BIN% --build . --config Debug
-%CMAKE_BIN% --build . --target install --config Debug
-%CMAKE_BIN% --build . --config Release
-%CMAKE_BIN% --build . --target install --config Release
+REM echo --- Building ogre-next-deps ---
+REM %CMAKE_BIN% -G %GENERATOR% -A %PLATFORM% ..
+REM %CMAKE_BIN% --build . --config Debug
+REM %CMAKE_BIN% --build . --target install --config Debug
+REM %CMAKE_BIN% --build . --config Release
+REM %CMAKE_BIN% --build . --target install --config Release
 
-cd ../../
-IF NOT EXIST ogre-next (
-	echo --- Cloning Ogre v2-2 ---
-	git clone --branch %OGRE_BRANCH_NAME% https://github.com/OGRECave/ogre-next
-)
-cd ogre-next
-IF NOT EXIST Dependencies (
-	mklink /D Dependencies ..\ogre-next-deps\build\ogredeps
-	IF ERRORLEVEL 1 (
-		echo Failed to create Dependency directory symlink. Run the script as Administrator.
-		EXIT /B 1
-	)
-)
+cd ../..
+
 mkdir build
 cd build
 echo --- Running CMake configure ---
-%CMAKE_BIN% -D OGRE_USE_BOOST=0 -D OGRE_CONFIG_THREAD_PROVIDER=0 -D OGRE_CONFIG_THREADS=0 -D OGRE_BUILD_COMPONENT_SCENE_FORMAT=1 -D OGRE_BUILD_SAMPLES2=1 -D OGRE_BUILD_TESTS=1 -D OGRE_DEPENDENCIES_DIR=..\..\ogre-next-deps\build\ogredeps -G %GENERATOR% -A %PLATFORM% ..
+%CMAKE_BIN% -D OGRE_USE_BOOST=0 -D OGRE_CONFIG_THREAD_PROVIDER=0 -D OGRE_CONFIG_THREADS=0 -D OGRE_BUILD_COMPONENT_SCENE_FORMAT=1 -D OGRE_BUILD_SAMPLES2=1 -D OGRE_BUILD_TESTS=1 -D OGRE_DEPENDENCIES_DIR=..\ogre-next-deps\build\ogredeps -G %GENERATOR% -A %PLATFORM% ..\..\card_1\ogre-next
 echo --- Building Ogre (Debug) ---
 %CMAKE_BIN% --build . --config Debug
 %CMAKE_BIN% --build . --target install --config Debug
